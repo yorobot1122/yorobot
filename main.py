2dd5b0d4f901fa01cf1ba7c54de11659f11ac158
yorobot1122 <vv22vx22@gmail.com>
2025-07-08 12:44:00 +0900

    Update main.py

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ YTDL_OPTIONS = {
 }
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-FFMPEG_PATH = os.path.join(BASE_DIR, "ffmpeg", "bin", "ffmpeg")
+FFMPEG_PATH = os.path.join(BASE_DIR, "bin", "ffmpeg")
 
 FFMPEG_OPTIONS = {
     'before_options':
