0757a0ad1407be10aa22ed9f16380836506ce7eb
yorobot1122 <vv22vx22@gmail.com>
2025-07-08 12:34:10 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-apt update
-apt install -y ffmpeg
+mkdir -p bin
+curl -L https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz | tar -xJ
+mv ffmpeg-*-static/ffmpeg ./bin/ffmpeg
+chmod +x ./bin/ffmpeg
